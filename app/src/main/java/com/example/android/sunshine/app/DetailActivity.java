9db8b186b095849a72414489e2aabd1b4f890fec
Brandon Miller <Brandon.Miller@BlueSombrero.com>
2015-12-16 18:58:31 -0500

    Imported correct SettingsActivity.java from udacity\'s code

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -61,7 +61,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            //startActivity(new Intent(this,SettingsActivity.class));
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
